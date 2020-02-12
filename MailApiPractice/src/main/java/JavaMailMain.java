3ebc2920e765696cbb41687f7c973427fff72811
LeBoot <brlebout@gmail.com>
2020-02-12 14:00:28 -0500

    Week 11

--- MailApiPractice/src/main/java/JavaMailMain.java
+++ MailApiPractice/src/main/java/JavaMailMain.java
@@ -1,22 +1,23 @@
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package bl.thesymmetrypodcast;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  *
  * @author Boone
  */
-@SpringBootApplication
-public class App {
+public class JavaMailMain {
     
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        try {
+            JavaMailUtil.sendEmail("BenLeBoot@gmail.com");
+        } catch (Exception ex) {
+            System.out.println("Error.  Location 2.");
+        }
     }
     
 }
